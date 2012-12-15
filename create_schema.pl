8b523c5945d4e0522a14ddae4910688fd878b97f
eric.rost@mybabylon.net <eric@192.168.1.37>
2012-12-15 17:56:20 -0600

    initial dev work

--- create_schema.pl
+++ create_schema.pl
@@ -1,16 +1,16 @@
  #!/usr/bin/perl
  # in a script
   use DBIx::Class::Schema::Loader qw/ make_schema_at /;
-  my $DB_Database="blog";
-	my $DB_Host="ziggurat";
+  my $DB_Database="Tracker";
+	my $DB_Host="localhost";
 	my $user="root";
-	my $password="GU-eYu3ujAje!het";
+	my $password="TR-tRa7pecR8";
 	####################################################
 	## This section should come from xml config later ##
 	####################################################
 	my $connectstr = "dbi:mysql:host=$DB_Host;database=$DB_Database";
   make_schema_at(
-      'BlogDB::Schema',
+      'AppDB::Schema',
       { dump_directory => './lib',
       },
       [ $connectstr, $user, $password
--- lib/App/Schema.pm
+++ lib/App/Schema.pm
@@ -1,5 +1,5 @@
 use utf8;
-package AppDB::Schema;
+package App::Schema;
 
 # Created by DBIx::Class::Schema::Loader
 # DO NOT MODIFY THE FIRST PART OF THIS FILE
@@ -12,8 +12,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_namespaces;
 
 
-# Created by DBIx::Class::Schema::Loader v0.07033 @ 2012-12-15 16:27:01
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:WZPfzAxFJfkZZkrJ4Hl2aw
+# Created by DBIx::Class::Schema::Loader v0.07033 @ 2012-12-15 16:23:20
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:aswiyUL5q1cwZU9cLTvUsw
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
